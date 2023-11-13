2b8a926e0959e120902314cec95c7767449550a1
lou-i0 <lothen123@googlemail.com>
2023-11-13 12:07:30 +0000

    got to draw lines working in canvas box

---
+++ web/js/draw.js
@@ -0,0 +1,31 @@
+
+
+//init object
+const draw = {};
+
+draw.path = (ctx, path, color = "black")=>
+{
+    ctx.strokeStyle = color;    // define colour of line 
+    ctx.lineWidth = 3;          // define width of line 
+    ctx.beginPath();
+    ctx.moveTo(...path[0]); // path[0] is x, y. the "..." spreads path into two sep components. 
+
+    for(let i=1; i < path.length; i++)
+    {
+        ctx.lineTo(...path[i]);
+
+
+    }
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.stroke();
+}
+
+
+draw.paths=(ctx, paths, color="black")=>
+{
+    for (const path of paths) 
+    {
+        draw.path(ctx, path, color = "black");
+    }
+}
--- web/js/drawBox.js
+++ web/js/drawBox.js
@@ -17,7 +17,10 @@ class DrawBox{
 
                     // draw onto the canvas
                     //=========================
-                    this.ctx = this.container.getContext("2d"); // to draw in 2d space context
+                    this.ctx = this.canvas.getContext("2d"); // to draw in 2d space context
+
+                    this.path = [];
+                    this.isDrawing=false;
 
                     // private method: listen for mouse events
                     //--------------------------
@@ -26,15 +29,59 @@ class DrawBox{
                 }
 
                 // Private methods to listen to mouse events. Cannot be called.
+                //=========================
                 #addEventListeners()
                 {
-                    this.canvas.onmousedown = (evt) =>  {
-                                                            const rect = this.canvas.getBoundingClientRect();
-                                                            const mouse_coords = [
-                                                                                    evt.clientX - rect.lef
-                                                                                    ,evt.clientY - rect.top
-                                                                                 ];
-                                                            console.log(mouse_coords);
-                                                        }
+                    // will record coords of mouse click
+                    //--------------------------
+                    this.canvas.onmousedown = (evt) =>  
+                    {   
+                        //call get_mouse function to retrieve coords
+                        const mouse_coords = this.#get_mouse(evt);
+
+                        //console.log(mouse_coords);
+                        this.path=[mouse_coords];
+                        this.isDrawing=true;                        
+                    }
+
+                    // only process when we are drawing 
+                    //--------------------------
+                    this.canvas.onmousemove = (evt) =>  
+                    {
+                        if(this.isDrawing)
+                        {
+                            //call get_mouse function to retrieve coords
+                            const mouse_coords = this.#get_mouse(evt);
+
+                            // console.log(mouse_coords);
+                            this.path.push(mouse_coords);
+                            //console.log(this.path.length);   
+                            
+                            this.#redraw();
+                        }
                 }
+                    // essentially end line drawn.
+                    //--------------------------
+                    this.canvas.onmouseup =()=> {this.isDrawing=false;}
+                }
+                
+
+                // Method to retrieve coords
+                //=========================
+                #get_mouse=(evt)=>
+                {
+                    const rect = this.canvas.getBoundingClientRect();
+                    return [
+                                Math.round(evt.clientX - rect.left) // x coord based on left of canvas rectangle.
+                                ,Math.round(evt.clientY - rect.top) // y coord based on top of canvas rectangle.
+                           ];
+                }
+
+                #redraw()
+                {
+                    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height); //clear canvas
+
+                    draw.path(this.ctx, this.path);
+                }
+
             }
